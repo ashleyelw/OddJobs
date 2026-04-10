686841c225a34b76ec1dd91f641f1ab168689ba8
ashleyelw <ashleyelworthy@gmail.com>
2026-04-10 13:01:07 +1000

    Player can select any flowers from garden
    Flowers in the garden are now constant, when the player selects a flower it automatically spawns in the assembly scene

---
+++ Assets/Scripts/CollectFlower.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class CollectFlower : MonoBehaviour
+{
+    public GameObject prefabReference; 
+
+    private bool playerInRange = false;
+
+    void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            Collect();
+        }
+    }
+
+    void Collect()
+    {
+        if (prefabReference != null)
+        {
+            FlowerTransferManager.Instance.selectedFlowerPrefabs.Add(prefabReference);
+            Debug.Log("Collected: " + prefabReference.name);
+        }
+        else
+        {
+            Debug.LogWarning("Missing prefabReference on " + gameObject.name);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+            playerInRange = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+            playerInRange = false;
+    }
+
+}
