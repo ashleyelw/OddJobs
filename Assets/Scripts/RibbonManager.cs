4b8457b4a4e3b41e2a8d0960f14f17188ca78bc9
alf-uts <alfonso.dolina@student.uts.edu.au>
2026-04-21 15:26:12 +1000

    Updates for Ribbons

--- Assets/Scripts/RibbonManager.cs
+++ Assets/Scripts/RibbonManager.cs
@@ -16,7 +16,7 @@ public class RibbonManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
+            DontDestroyOnLoad(gameObject); // ADD THIS
         }
         else
         {
--- Assets/Scripts/RibbonSpawner.cs
+++ Assets/Scripts/RibbonSpawner.cs
@@ -2,14 +2,24 @@ using UnityEngine;
 
 public class RibbonSpawner : MonoBehaviour
 {
-  public Transform bouquetPoint;
+    public Transform bouquetPoint;
 
-   public void SpawnRibbon(GameObject ribbonPrefab)
-   {
-    if(ribbonPrefab!=null)
+    public void SpawnRibbon(GameObject ribbonPrefab)
     {
-        Instantiate(ribbonPrefab,bouquetPoint.position,Quaternion.identity);
-        
+        if (ribbonPrefab != null)
+        {
+            Instantiate(ribbonPrefab, bouquetPoint.position, Quaternion.identity);
+
+            // Tell RibbonManager which ribbon was selected
+            if (RibbonManager.Instance != null)
+            {
+                RibbonManager.Instance.SelectRibbon(ribbonPrefab);
+                Debug.Log($"[RibbonSpawner] Ribbon selected: {ribbonPrefab.name}");
+            }
+            else
+            {
+                Debug.LogWarning("[RibbonSpawner] RibbonManager.Instance is null!");
+            }
+        }
     }
-   }
 }
